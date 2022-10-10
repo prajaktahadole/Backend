67402867d5cd1d08cbe29bfc0f171a244900dc35
prajaktahadole <prajaktahadole@gmail.com>
2022-10-10 23:50:57 +0530

    Create new.js

---
+++ MONGODB ADVANCED/new.js
@@ -0,0 +1,734 @@
+// mongo> show dbs
+// admin     40.00 KiB
+// config    60.00 KiB
+// local     88.00 KiB
+// practice  80.00 KiB
+// mongo> use practice
+// switched to db practice
+// practice> show collections
+// data
+// data2
+// practice> db.data.find({})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+    id: 6,
+    first_name: 'Flossie',
+    last_name: 'Confait',
+    email: 'fconfait5@last.fm',
+    gender: 'Female',
+    ip_address: '196.175.192.168'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b5"),
+    id: 7,
+    first_name: 'Irvine',
+    last_name: 'Monkton',
+    email: 'imonkton6@cnn.com',
+    gender: 'Bigender',
+    ip_address: '44.220.131.16'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b6"),
+    id: 8,
+    first_name: 'Frannie',
+    last_name: 'Averall',
+    email: 'faverall7@sciencedaily.com',
+    gender: 'Male',
+    ip_address: '115.201.192.112'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b7"),
+    id: 9,
+    first_name: 'Kendall',
+    last_name: 'De Lisle',
+    email: 'kdelisle8@ow.ly',
+    gender: 'Agender',
+    ip_address: '12.235.46.165'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b8"),
+    id: 10,
+    first_name: 'Orlan',
+    last_name: 'Count',
+    email: 'ocount9@soup.io',
+    gender: 'Male',
+    ip_address: '45.231.170.73'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b9"),
+    id: 11,
+    first_name: 'Milissent',
+    last_name: 'Koeppke',
+    email: 'mkoeppkea@usda.gov',
+    gender: 'Polygender',
+    ip_address: '255.161.240.21'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842ba"),
+    id: 12,
+    first_name: 'Ileane',
+    last_name: 'Riddle',
+    email: 'iriddleb@princeton.edu',
+    gender: 'Non-binary',
+    ip_address: '160.12.54.29'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bb"),
+    id: 13,
+    first_name: 'Kristyn',
+    last_name: 'Liptrod',
+    email: 'kliptrodc@vk.com',
+    gender: 'Female',
+    ip_address: '81.105.165.223'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bc"),
+    id: 14,
+    first_name: 'Aldwin',
+    last_name: 'Dahler',
+    email: 'adahlerd@moonfruit.com',
+    gender: 'Male',
+    ip_address: '90.229.221.51'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bd"),
+    id: 15,
+    first_name: 'Levy',
+    last_name: 'McEvon',
+    email: 'lmcevone@walmart.com',
+    gender: 'Male',
+    ip_address: '56.182.205.163'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842be"),
+    id: 16,
+    first_name: 'Homerus',
+    last_name: 'Musker',
+    email: 'hmuskerf@icq.com',
+    gender: 'Male',
+    ip_address: '167.248.192.185'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bf"),
+    id: 17,
+    first_name: 'Clyde',
+    last_name: 'McGloughlin',
+    email: 'cmcgloughling@bbc.co.uk',
+    gender: 'Male',
+    ip_address: '122.159.144.48'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c0"),
+    id: 18,
+    first_name: 'Amabel',
+    last_name: 'Tyhurst',
+    email: 'atyhursth@nih.gov',
+    gender: 'Female',
+    ip_address: '157.122.55.20'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c1"),
+    id: 19,
+    first_name: 'Esra',
+    last_name: 'Loxly',
+    email: 'eloxlyi@businessweek.com',
+    gender: 'Male',
+    ip_address: '229.75.74.173'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c2"),
+    id: 20,
+    first_name: 'Thomasin',
+    last_name: 'Karoly',
+    email: 'tkarolyj@e-recht24.de',
+    gender: 'Female',
+    ip_address: '231.216.20.166'
+  }
+]
+// practice> db.data.find({id : {$not : {$gte : 10}}})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+
+// practice> db.data.find({id: {$lte : 10}})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+
+// practice> db.data.find({id: {$lte : 10}})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+    id: 6,
+    first_name: 'Flossie',
+    last_name: 'Confait',
+    email: 'fconfait5@last.fm',
+    gender: 'Female',
+    ip_address: '196.175.192.168'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b5"),
+    id: 7,
+    first_name: 'Irvine',
+    last_name: 'Monkton',
+    email: 'imonkton6@cnn.com',
+    gender: 'Bigender',
+    ip_address: '44.220.131.16'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b6"),
+    id: 8,
+    first_name: 'Frannie',
+    last_name: 'Averall',
+    email: 'faverall7@sciencedaily.com',
+    gender: 'Male',
+
+// practice> db.data.find({})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+    id: 6,
+    first_name: 'Flossie',
+    last_name: 'Confait',
+    email: 'fconfait5@last.fm',
+    gender: 'Female',
+    ip_address: '196.175.192.168'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b5"),
+    id: 7,
+    first_name: 'Irvine',
+    last_name: 'Monkton',
+    email: 'imonkton6@cnn.com',
+    gender: 'Bigender',
+    ip_address: '44.220.131.16'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b6"),
+    id: 8,
+    first_name: 'Frannie',
+    last_name: 'Averall',
+    email: 'faverall7@sciencedaily.com',
+    gender: 'Male',
+    ip_address: '115.201.192.112'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b7"),
+    id: 9,
+    first_name: 'Kendall',
+    last_name: 'De Lisle',
+    email: 'kdelisle8@ow.ly',
+    gender: 'Agender',
+    ip_address: '12.235.46.165'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b8"),
+    id: 10,
+    first_name: 'Orlan',
+    last_name: 'Count',
+    email: 'ocount9@soup.io',
+    gender: 'Male',
+    ip_address: '45.231.170.73'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b9"),
+    id: 11,
+    first_name: 'Milissent',
+    last_name: 'Koeppke',
+    email: 'mkoeppkea@usda.gov',
+    gender: 'Polygender',
+    ip_address: '255.161.240.21'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842ba"),
+    id: 12,
+    first_name: 'Ileane',
+    last_name: 'Riddle',
+    email: 'iriddleb@princeton.edu',
+    gender: 'Non-binary',
+    ip_address: '160.12.54.29'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bb"),
+    id: 13,
+    first_name: 'Kristyn',
+    last_name: 'Liptrod',
+    email: 'kliptrodc@vk.com',
+    gender: 'Female',
+    ip_address: '81.105.165.223'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bc"),
+    id: 14,
+    first_name: 'Aldwin',
+    last_name: 'Dahler',
+    email: 'adahlerd@moonfruit.com',
+    gender: 'Male',
+    ip_address: '90.229.221.51'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bd"),
+    id: 15,
+    first_name: 'Levy',
+    last_name: 'McEvon',
+    email: 'lmcevone@walmart.com',
+    gender: 'Male',
+    ip_address: '56.182.205.163'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842be"),
+    id: 16,
+    first_name: 'Homerus',
+    last_name: 'Musker',
+    email: 'hmuskerf@icq.com',
+    gender: 'Male',
+    ip_address: '167.248.192.185'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842bf"),
+    id: 17,
+    first_name: 'Clyde',
+    last_name: 'McGloughlin',
+    email: 'cmcgloughling@bbc.co.uk',
+    gender: 'Male',
+    ip_address: '122.159.144.48'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c0"),
+    id: 18,
+    first_name: 'Amabel',
+    last_name: 'Tyhurst',
+    email: 'atyhursth@nih.gov',
+    gender: 'Female',
+    ip_address: '157.122.55.20'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c1"),
+    id: 19,
+    first_name: 'Esra',
+    last_name: 'Loxly',
+    email: 'eloxlyi@businessweek.com',
+    gender: 'Male',
+    ip_address: '229.75.74.173'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842c2"),
+    id: 20,
+    first_name: 'Thomasin',
+    last_name: 'Karoly',
+    email: 'tkarolyj@e-recht24.de',
+    gender: 'Female',
+    ip_address: '231.216.20.166'
+  }
+]
+
+// practice> db.data.find({id : {$lte : 10}})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    id: 1,
+    first_name: 'Eb',
+    last_name: 'Jeaves',
+    email: 'ejeaves0@yellowbook.com',
+    gender: 'Male',
+    ip_address: '62.240.10.180'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    id: 2,
+    first_name: 'Carmela',
+    last_name: 'Long',
+    email: 'clong1@ted.com',
+    gender: 'Agender',
+    ip_address: '5.52.80.91'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    id: 3,
+    first_name: 'Gail',
+    last_name: 'Eaken',
+    email: 'geaken2@istockphoto.com',
+    gender: 'Male',
+    ip_address: '83.252.56.198'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    id: 4,
+    first_name: 'Irv',
+    last_name: 'MacAdam',
+    email: 'imacadam3@guardian.co.uk',
+    gender: 'Male',
+    ip_address: '194.208.227.145'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    id: 5,
+    first_name: 'Titos',
+    last_name: 'Geddes',
+    email: 'tgeddes4@seesaa.net',
+    gender: 'Male',
+    ip_address: '28.99.76.2'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+    id: 6,
+    first_name: 'Flossie',
+    last_name: 'Confait',
+    email: 'fconfait5@last.fm',
+    gender: 'Female',
+    ip_address: '196.175.192.168'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b5"),
+    id: 7,
+    first_name: 'Irvine',
+    last_name: 'Monkton',
+    email: 'imonkton6@cnn.com',
+    gender: 'Bigender',
+    ip_address: '44.220.131.16'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b6"),
+    id: 8,
+    first_name: 'Frannie',
+    last_name: 'Averall',
+    email: 'faverall7@sciencedaily.com',
+    gender: 'Male',
+    ip_address: '115.201.192.112'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b7"),
+    id: 9,
+    first_name: 'Kendall',
+    last_name: 'De Lisle',
+    email: 'kdelisle8@ow.ly',
+    gender: 'Agender',
+    ip_address: '12.235.46.165'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b8"),
+    id: 10,
+    first_name: 'Orlan',
+    last_name: 'Count',
+    email: 'ocount9@soup.io',
+    gender: 'Male',
+    ip_address: '45.231.170.73'
+  }
+]
+// practice> db.data.find({id : {$lte : 10}}, {first_name : 1})
+[
+  { _id: ObjectId("633c2cbda227a700d80842af"), first_name: 'Eb' },
+  { _id: ObjectId("633c2cbda227a700d80842b0"), first_name: 'Carmela' },
+  { _id: ObjectId("633c2cbda227a700d80842b1"), first_name: 'Gail' },
+  { _id: ObjectId("633c2cbda227a700d80842b2"), first_name: 'Irv' },
+  { _id: ObjectId("633c2cbda227a700d80842b3"), first_name: 'Titos' },
+  { _id: ObjectId("633c2cbda227a700d80842b4"), first_name: 'Flossie' },
+  { _id: ObjectId("633c2cbda227a700d80842b5"), first_name: 'Irvine' },
+  { _id: ObjectId("633c2cbda227a700d80842b6"), first_name: 'Frannie' },
+  { _id: ObjectId("633c2cbda227a700d80842b7"), first_name: 'Kendall' },
+  { _id: ObjectId("633c2cbda227a700d80842b8"), first_name: 'Orlan' }
+]
+// practice> db.data.find({id : {$lte : 10}}, {first_name : 1}, {gender : 1})
+[
+  { _id: ObjectId("633c2cbda227a700d80842af"), first_name: 'Eb' },
+  { _id: ObjectId("633c2cbda227a700d80842b0"), first_name: 'Carmela' },
+  { _id: ObjectId("633c2cbda227a700d80842b1"), first_name: 'Gail' },
+  { _id: ObjectId("633c2cbda227a700d80842b2"), first_name: 'Irv' },
+  { _id: ObjectId("633c2cbda227a700d80842b3"), first_name: 'Titos' },
+  { _id: ObjectId("633c2cbda227a700d80842b4"), first_name: 'Flossie' },
+  { _id: ObjectId("633c2cbda227a700d80842b5"), first_name: 'Irvine' },
+  { _id: ObjectId("633c2cbda227a700d80842b6"), first_name: 'Frannie' },
+  { _id: ObjectId("633c2cbda227a700d80842b7"), first_name: 'Kendall' },
+  { _id: ObjectId("633c2cbda227a700d80842b8"), first_name: 'Orlan' }
+]
+// practice> db.data.find({id : {$lte : 10}}, {first_name : 1, gender : 1})
+[
+  {
+    _id: ObjectId("633c2cbda227a700d80842af"),
+    first_name: 'Eb',
+    gender: 'Male'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b0"),
+    first_name: 'Carmela',
+    gender: 'Agender'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b1"),
+    first_name: 'Gail',
+    gender: 'Male'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b2"),
+    first_name: 'Irv',
+    gender: 'Male'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b3"),
+    first_name: 'Titos',
+    gender: 'Male'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b4"),
+    first_name: 'Flossie',
+    gender: 'Female'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b5"),
+    first_name: 'Irvine',
+    gender: 'Bigender'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b6"),
+    first_name: 'Frannie',
+    gender: 'Male'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b7"),
+    first_name: 'Kendall',
+    gender: 'Agender'
+  },
+  {
+    _id: ObjectId("633c2cbda227a700d80842b8"),
+    first_name: 'Orlan',
+    gender: 'Male'
+  }
+]
+// practice> db.data.find({id : {$lte : 10}}, {first_name : 1, gender : 1, _id : 0})
+[
+  { first_name: 'Eb', gender: 'Male' },
+  { first_name: 'Carmela', gender: 'Agender' },
+  { first_name: 'Gail', gender: 'Male' },
+  { first_name: 'Irv', gender: 'Male' },
+  { first_name: 'Titos', gender: 'Male' },
+  { first_name: 'Flossie', gender: 'Female' },
+  { first_name: 'Irvine', gender: 'Bigender' },
+  { first_name: 'Frannie', gender: 'Male' },
+  { first_name: 'Kendall', gender: 'Agender' },
+  { first_name: 'Orlan', gender: 'Male' }
+]
